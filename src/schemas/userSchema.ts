5820ea8c3bbc266e3dd245dbcc0181382842943e
Maria <mariabevenuto062@gmail.com>
2023-04-21 19:11:58 -0300

    fix: confirm password is required now

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -3,5 +3,5 @@ import joi from "joi";
 export const signUpSchema = joi.object({
     email: joi.string().email().max(30).required(),
     password: joi.string().min(9).required(),
-    confirmPassword: joi.ref('password')
+    confirmPassword: joi.string().valid(joi.ref("password")).required()
 })
